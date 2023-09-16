ae2417bab0c343aa38daf1eb1054ef641e4af792
Akili Daniels <akili.daniels@att.net>
2023-09-16 16:25:26 -0700

    Added Encore & Stimulus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: 'GET')]
     public function home(): Response
     {
-        $welcome = 'Welcome to your new website Akili Daniels';
+        $welcome = 'Welcome! To your new website Akili Daniels';
 
-        return new Response($welcome);
+        return $this->render('home/home.html.twig', [
+            'welcome' => $welcome,
+        ]);
     }
 }
