9c67dd33d2440310cfe45c06ad4736cb52996015
Akili Daniels <akili.daniels@att.net>
2023-11-16 20:53:58 -0800

    Modified assets path

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// custom application javascript file
+import './javascript/app.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
