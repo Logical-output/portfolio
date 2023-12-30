b7da3870e9d904dbb3ee9a6ce3df5de02a501be5
Akili Daniels <akili.daniels@att.net>
2023-12-30 09:19:27 -0800

    modified name to our javascript file

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,6 @@ import './bootstrap.js';
  */
 
 // custom application javascript file
-import './javascript/app.js';
+import './javascript/home.js';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
